073a08810aca42d102012084ff93b405facc1740
Florian COURTAIGNE <f.courtaigne@gmail.com>
2022-06-17 15:42:54 +0200

    working on fixtures / faker

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public const CATEGORIES = [
+        'Coiffures',
+        'Soins du corps',
+        'Soins du visage',
+        'Make Up',
+        'Manucure & Nail design'
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $this->addReference('category_' . $categoryName, $category);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/ServiceFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Service;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
+
+class ServiceFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            $service = new Service();
+            $service->setName($faker->paragraphs(1, true));
+            $service->setCategory($this->getReference('category_' . $faker->numberBetween(1, 5)));
+            $this->addReference('service_' . $i, $service);
+
+            $manager->persist($service);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
