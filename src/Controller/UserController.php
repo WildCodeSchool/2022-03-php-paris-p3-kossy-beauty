5a7e61dd2f0022a6e87bac672c25935d291cb56e
Val <titi-phenom-wcs@outlook.fr>
2022-06-22 14:55:30 +0200

    role management for users profil page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,7 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    #[IsGranted('ROLE_USER')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -23,43 +23,67 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+    // #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request, UserRepository $userRepository): Response
+    // {
+    //     $user = new User();
+    //     $form = $this->createForm(UserType::class, $user);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->add($user, true);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $userRepository->add($user, true);
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
+    //         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    //     }
 
-        return $this->renderForm('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
+    //     return $this->renderForm('user/new.html.twig', [
+    //         'user' => $user,
+    //         'form' => $form,
+    //     ]);
+    // }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
     public function show(User $user): Response
     {
+        $currentUser = $this->getUser();
+        if (!$this->hasProfileAccess($user, $currentUser)) {
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $role = 'admin';
+        } elseif (in_array('ROLE_PROVIDER', $user->getRoles())) {
+            $role = 'provider';
+        } else {
+            $role = 'utilisateur';
+        }
         return $this->render('user/show.html.twig', [
-            'user' => $user,
+            'user'  => $user,
+            'role'  => $role,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
     public function edit(Request $request, User $user, UserRepository $userRepository): Response
     {
-        $form = $this->createForm(UserType::class, $user);
+        $currentUser = $this->getUser();
+        if (!$this->hasProfileAccess($user, $currentUser)) {
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+        $form = $this->createForm(
+            UserType::class,
+            $user,
+            [
+                'role' => $currentUser->getRoles()
+            ]
+        );
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user, true);
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_show', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('user/edit.html.twig', [
@@ -76,5 +100,20 @@ class UserController extends AbstractController
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // Check if an user is authorized to access to a profil page
+    public function hasProfileAccess(User $user, $currentUser): bool
+    {
+        $userId = $user->getId();
+        $currentUserId = $currentUser->getId();
+        $currentUserRole = $currentUser->getRoles();
+        if (
+            !in_array('ROLE_ADMIN', $currentUserRole) &&
+            $currentUserId !== $userId
+        ) {
+            return false;
+        }
+        return true;
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,27 +11,39 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Display common fields for every users
         $builder
             ->add('telephone')
-            ->add('roles')
-            ->add('password')
+            // ->add('password')
             ->add('firstname')
             ->add('lastname')
-            ->add('companyName')
             ->add('email')
             ->add('town')
             ->add('district')
             ->add('image')
-            ->add('isTop')
-            ->add('isArchived')
-            ->add('companyDescription')
         ;
+
+        // Display additionnal fields for a specific role
+        if (in_array('ROLE_ADMIN', $options['role'])) {
+            $builder
+                ->add('roles')
+                ->add('isTop')
+                ->add('isArchived')
+            ;
+        }
+        if (in_array('ROLE_PROVIDER', $options['role'])) {
+            $builder
+                ->add('companyName')
+                ->add('companyDescription')
+            ;
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'role' => ['ROLE_USER']
         ]);
     }
 }
