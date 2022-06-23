56631e12c8a4a5fe88b4e755c32e9b6e828e9fdf
Val <titi-phenom-wcs@outlook.fr>
2022-06-23 10:29:46 +0200

    add control on input fields when editing a profile

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,11 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
