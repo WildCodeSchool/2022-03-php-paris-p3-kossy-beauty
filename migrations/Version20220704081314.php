bc0b9733d0f339569882aaaba63a9ed0677cbb9c
Nolan Prévost <nolan.prevost@gmail.com>
2022-07-04 10:14:34 +0200

    commit test

--- migrations/Version20220704081314.php
+++ migrations/Version20220704081314.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220629093813 extends AbstractMigration
+final class Version20220704081314 extends AbstractMigration
 {
     public function getDescription(): string
     {
