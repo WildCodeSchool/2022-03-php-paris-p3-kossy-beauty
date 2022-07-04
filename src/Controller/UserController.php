e8e467a96617298a6d7ae2f3b18e0b09e412ca41
Val <titi-phenom-wcs@outlook.fr>
2022-07-04 23:21:13 +0200

    delete use statement for twig in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Twig\Environment;
 
 #[Route('/user')]
 class UserController extends AbstractController
